19f9f7c374b4b3cc41dede0dbdb820720008e5b1
Andrey <liamo19@mail.ru>
2022-07-07 19:22:25 +0600

    Added strcmp function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,49 @@
 #include <stdio.h>
+
+
 void func_puts(const char *new_str)
 {
-    int i = 0;
-    while (*(new_str + i) != '\0')
+    int elem = 0;
+    while (*(new_str + elem) != '\0')
     {
-        printf("%c", *(new_str + (i)));
-        i++;
+        printf("%c", *(new_str + elem));
+        elem++;
     }
     printf("\n");
 }
+
+int func_strcmp(const char *new_str_1, const char *new_str_2)
+{
+    for (int j = 0; *(new_str_1 + j) != '\0' || *(new_str_2 + j) != '\0'; j++)
+    {
+        if(*(new_str_1 + j) != *(new_str_2 + j))
+        {
+            if(*(new_str_1 + j) > *(new_str_2 + j))
+            {
+                return 1;
+            }
+            if (*(new_str_1 + j) < *(new_str_2 + j))
+            {
+                return -1;
+            }
+        }
+        if(*(new_str_1 + j) != '\0' && *(new_str_2 + j) == '\0')
+        {
+            return 1;
+        }
+        if (*(new_str_1 + j) == '\0' && *(new_str_2 + j) != '\0')
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+
 int main()
 {
-    char str[256] = "my_name";
-    func_puts(str);
+    char str_1[256] = "andreytttttt";
+    char str_2[256] = "andreytttt";
+    int a = func_strcmp(str_1, str_2);
+    printf("%d\n", a);
 }
